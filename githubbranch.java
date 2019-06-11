7d6cf1cfc3c4436d086746cf7e06a19863080a84
ssvelm <45192568+ssvelm@users.noreply.github.com>
2019-06-11 11:33:16 +0530

    #SAA-I19
    My new commit from Github app
    in Mac

---
+++ githubbranch.java
@@ -0,0 +1,59 @@
+<div style="width:100%;border:1px solid #ffffff;height:auto;">
+  <table cellspacing="0" cellpadding="0" style="table-layout: fixed;width:100%;">
+    <tbody>
+      <tr align="left">
+        <td style="width:45%;height:300px;background: #3fa8ad;" align="center"><table cellspacing="0" cellpadding="0" style="width:100%;table-layout: fixed;border-spacing: 13px;">
+            <tbody>
+              <tr>
+                <td style="width:100%;border: 1px solid #1a8287;height:610px;box-shadow: 0 0 1px 0px ##b2d2d4;padding:30px;text-align:center;background: #1A8287;" align="center" class=""><div style="color: #fff;width:100%;font-weight: 500;font-size:24px;line-height:28px;text-align:center;margin:0px auto;font-family:avenir;margin-bottom:30px;">Report Status</div>
+                  <div style="color: #fff;width:100%;line-height:28px;font-size:18px;text-align:center;margin:0px auto;font-family:avenir;"> The feature <span style="color: #ffba24;font-weight: 450;">Docs team API response </span>update is currently now at testing status <span style="color:#fbc531">On Hold</span></div></td>
+              </tr>
+            </tbody>
+          </table></td>
+        <td style="width:55%;padding-top: 15px;border-right: 2px solid #99c7ca;border-top: 2px solid #99c7ca;border-bottom: 2px solid #99c7ca;vertical-align: top;" align="left;"><table cellspacing="0" cellpadding="0">
+            <tbody>
+              <tr style="width:100%;border-bottom: 1px dashed #ccc !important;">
+                <td align="left" style="padding-left:30px;"><div style="display:inline-block;font-family:avenir;color:#000000;font-weight:700;    margin-top:21px;margin-bottom:1px;font-size:16px;"> Developer : </div>
+                  <br>
+                  <div style="padding-top: 0px;width:90%;display:block;line-height:20px;font-family:avenir;word-break:break-word;font-size:16px;"> Kubendran M </div></td>
+              </tr>
+              <tr style="width:100%;border-bottom: 1px dashed #ccc;">
+                <td align="left" style="padding-left:30px;"><div style="display:inline-block;font-family:avenir;color:#000000;margin-top: 25px;margin-bottom: -1px;font-size:16px;font-weight:700;"> Local zoho to test : </div>
+                  <br>
+                  <div style="padding-top: 0px;display:block;width:90%;line-height:20px;font-family:avenir;word-break:break-word;font-size:16px;"> Yet to be decided </div></td>
+              </tr>
+              <tr style="margin-bottom:30px;   border-bottom: 1px dashed #ccc;">
+                <td align="left" style="padding-left:30px;"><div style="display:inline-block;font-family:avenir;color:#000000;margin-top: 25px;font-size:16px;font-weight:700;"> Type of change : </div>
+                  <br>
+                  <div style="padding-top: 0px;display:block;width:90%;line-height:20px;font-family:avenir;word-break:break-word;margin-top: -2px;font-size:16px;"> Code Push </div></td>
+              </tr>
+              <tr style="margin-bottom:30px;   border-bottom: 1px dashed #ccc;">
+                <td align="left" style="padding-left:30px;"><div style=" display:inline-block;font-family:avenir;color:#000000;margin-top: 25px;font-size:16px;font-weight:700;"> Tester : </div>
+                  <br>
+                  <div style="padding-top: 0px;width:90%;display:block;line-height:20px;font-family:avenir;word-break:break-word;margin-top: -2px;font-size:16px;"> Janaki </div></td>
+              </tr>
+              <tr style="margin-bottom:30px;">
+                <td align="left" style="padding-left:30px;"><div style="display:inline-block;font-family:avenir;color:#000000;margin-top: 25px;font-size:16px;font-weight:700;"> Tester comments : </div>
+                  <br>
+                  <div style="padding-top: 0px;display:block;width:85%;line-height:27px;font-family:avenir;word-break:break-word;font-size:16px;"> Update the status once the build has been updated properly in a local </div></td>
+              </tr>
+              <tr style="margin-bottom:30px;   border-bottom: 1px dashed #ccc;">
+                <td align="left" style="padding-left:30px;"><div style="display:inline-block;font-family:avenir;color:#000000;margin-top: 25px;font-size:16px;font-weight:700;"> Issue Count : </div>
+                  <br>
+                  <div style="color:#ff6b6b;font-weight:600; padding-top: 0px;width:90%;display:block;line-height:20px;font-family:avenir;word-break:break-word;margin-top: -2px;font-size:16px;"> 130 </div></td>
+              </tr>
+              <tr style="margin-bottom:30px;   border-bottom: 1px dashed #ccc;">
+                <td align="left" style="padding-left:30px;"><div style="display:inline-block;font-family:avenir;color:#000000;text-align: center;margin-top: 25px;font-size:16px;font-weight:700;">
+                    <div class="inlineBlock fontsmoothauto" style="color: #fff;margin: 0px auto;text-align: center;font-family: avenir;font-size: 15px;width: 100px;text-align: center;background: #1A8287;box-sizing: border-box;padding: 8px;border: 1px solid #1A8287;margin: 0px auto;border-radius: 2px;margin-bottom:20px;cursor:pointer;">
+                        <a href="" style="text-decoration:none;cursor:pointer;color:#fff;font-family:avenir;">Click Here</a>
+                    </div>
+                  </div></td>
+              </tr>
+            </tbody>
+          </table></td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+</body>
+</html>
