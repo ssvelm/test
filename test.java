bf1199aff64918a5cf23f4afba6fa2208076aa5d
ssvelm <ssvelm@gmail.com>
2019-06-19 10:27:55 +0530

    #FS-I44 #comment Cannot fix this #priority High #status In_progress

--- test.java
+++ test.java
@@ -5,7 +5,7 @@ test
 
 
 
-#Ug-I10 #comment Cannot fix this #priority High #status In_progress #SAA-I47 #comment Cannot fix this #priority High #status In_progress
+#Ug-I10 #comment Cannot fix this #priority High #status In_progress 
 
 
 New one
